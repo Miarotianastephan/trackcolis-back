9fa1dfe1a6362f74fd2efec9433b8a927fd19aa0
Miarotiana Stephan <miarotianastephan01@gmail.com>
2025-11-21 13:35:26 +0300

    init express App

---
+++ src/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+require('dotenv').config();
+
+const app = express();
+
+// Middlewares de sécurité et utilitaires
+app.use(helmet());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Route de test
+app.get('/', (req, res) => {
+  res.json({ message: 'TrackColis Express Is Ready!' });
+});
+
+// Gestion des routes 404
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route non trouvée' });
+});
+
+// Gestion globale des erreurs
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Erreur serveur' });
+});
+
+module.exports = app;
