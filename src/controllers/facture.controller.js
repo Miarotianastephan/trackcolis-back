d07e905141bffbf97cd08f3a4c7d7d39beeabb4e
kagealan <kagealan33@gmail.com>
2026-05-22 13:59:18 +0300

    fix invoice user return

--- src/controllers/facture.controller.js
+++ src/controllers/facture.controller.js
@@ -26,7 +26,6 @@ async function getInvoiceByUserId(req, res, next){
     try {
         const { user_id } = req.params;
         const factures = await factureService.getInvoiceByUserId(user_id);
-        if (!factures || factures.length === 0) return res.status(404).json({ error: 'No invoices found for this user' });
         res.status(201).json({ factures });
     } catch (err) {
         next(err);
